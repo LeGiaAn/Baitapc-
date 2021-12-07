4ea3e543925e09b59c2dc2e568962334fb955d45
An <andoycle@gmail.com>
2021-12-07 08:37:43 +0700

    BAITAPC1

---
+++ BAITAPC1/BAITAPC1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace bailamc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(i * 5 + j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
